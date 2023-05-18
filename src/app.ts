98ccf5f9c953c50feab23c9c652b02f6ff7ef5f8
Javier Lim <javier_lim@ensigninfosecurity.com>
2023-05-18 14:50:37 +0800

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import api from "./api";
+import cors from "cors";
 
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 
 app.use("/", api);
 
